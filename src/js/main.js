6d127d63cd4085c3269dc2f8e8204b8536a665d9
marcosdavis <macrosdavy@protonmail.com>
2026-04-24 09:28:32 -0600

    Added javascript to update information on page with respect to specific parks. refined some styling to match the example website.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+// Update the href and park name in the disclaimer
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+// Update the title 
+const parkTitle = document.querySelector("head > title");
+parkTitle.innerHTML = parkData.fullName;
+
+// Update the hero banner image
+const heroImg = document.querySelector(".hero-ban > img");
+heroImg.src = parkData.images[0].url;
+
+// Update the title and subtitle of the hero
+const heroBanTitles = document.querySelector(".hero-ban-title-container");
+heroBanTitles.innerHTML = parkInfoTemplate(parkData);
+
+function parkInfoTemplate(info) {
+    return `<a href="#" class="hero-ban-title">${info.name}</a>
+    <p class="hero-ban-subtitles"> 
+        <span>${info.designation}</span>
+        <span>${info.states}</span>
+    </p>`;
+}
